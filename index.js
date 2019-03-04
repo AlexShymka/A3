39c88fa0147c707dc66a80cef46b83d66560fcc2
AlexShymka <androidaccountalex@gmail.com>
2019-03-04 12:17:12 -0700

    Added cookies and linux
    Got some semblence of cookies working and pushing to verify deployment on linux.

--- index.js
+++ index.js
@@ -2,20 +2,40 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var cookieParser = require('cookie-parser');
 
-app.use(express.static(__dirname)); // So CSS works
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+//app.use(express.static(__dirname)); // So CSS works
+app.use(cookieParser());
+let port = 3000;
 var i = 0;
 var userList = [];
 var chatHistory = [];
+
+app.get('/list', function(req, res){
+  res.send(req.cookies);
+});
+
+app.get('/', function(req, res){
+  console.log("Cookie Parsing");
+
+  if (req.cookies['SengA3']) {
+    console.log('Cookie found. Value: ' + req.cookies['SengA3']);
+  }
+  else {
+    res.cookie('SengA3', 0);
+    console.log("No Cookie found. Cookie Sent");
+  }
+
+  app.use(express.static(__dirname)); // So CSS works
+  res.sendFile(__dirname + '/index.html');
+})
 
 var weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
 io.on('connection', function(socket){
   socket.thisUser = 'user_'+ i;
+  socket.color = [000,000,000];
   userList.push(socket.thisUser);
 	console.log(socket.thisUser + ' connected');
   console.log(userList);
@@ -32,21 +52,65 @@ io.on('connection', function(socket){
   })
 
   socket.on('chat message', function(msg){
-    var msgdate = new Date();
-    var msgMins = msgdate.getMinutes();
-    if (msgMins < 10) {
-      msgMins = '0' + msgMins;
+    //Nickname Change
+    if (msg.startsWith("/nick ")) {
+      var newNick = msg.slice(6);
+      var oldNick = socket.thisUser;
+      console.log('Potential new nick: ' + newNick);
+      if (userList.indexOf(newNick) === -1) {
+        console.log('nickname is good');
+        userList.splice(userList.indexOf(oldNick), 1, newNick);
+        msg = oldNick + ' Changed their name to ' + newNick;
+        console.log(userList);
+        //Push changes
+        socket.thisUser = newNick;
+        io.emit('user update', userList);
+        chatHistory.push(msg);
+        io.emit('chat message', msg);
+      }
+      else {
+        console.log('nickname already exists');
+        socket.emit('chat message', 'That nickname is in use. Please try another');
+      }
     }
-    var msgHours = msgdate.getHours();
-    if (msgHours < 10) {
-      msgHours = '0' + msgHours;
+    //Change Color
+    else if (msg.startsWith("/nickcolor")){
+      var newclr = msg.slice(11);
+      if (newclr.length === 9) {
+        console.log('Changing color for ' + socket.thisUser);
+        socket.color = [newclr.substr(0,3), newclr.substr(3,3), newclr.substr(6,3)]
+        console.log(socket.color);
+        var colorFormat = '<span style=color:rgb(' + socket.color[0] + ',' + socket.color[1] + ',' + socket.color[2] + ')>';
+        socket.emit('chat message', 'You\'ve got a new color. It looks like ' + colorFormat + 'this!</span>');
+      }
+      else {
+        console.log('Bad format from color request by: ' + socket.thisUser);
+        socket.emit('chat message', 'Bad color format. RGB value must be 9 numbers. Preprend 0s to numbers under 100');
+      }
+
+
     }
-    var formattedDate = weekdays[msgdate.getDay()] + ' ' + months[msgdate.getMonth()] +' ' + msgdate.getDate() + ' ' + (msgdate.getYear() + 1900) + ' ' + msgHours + ':'+ msgMins +'; ';
-    msg = formattedDate + socket.thisUser + ": " + msg;
-    console.log('message: ' + msg);
-    socket.broadcast.emit('chat message', msg);
-    socket.emit('chat message', '<b>' + msg +'</b>' );
-    chatHistory.push(msg);
+    //Normal Message
+    else {
+      var msgdate = new Date();
+      var msgMins = msgdate.getMinutes();
+      if (msgMins < 10) {
+        msgMins = '0' + msgMins;
+      }
+      var msgHours = msgdate.getHours();
+      if (msgHours < 10) {
+        msgHours = '0' + msgHours;
+      }
+      var formattedDate = weekdays[msgdate.getDay()] + ' ' + months[msgdate.getMonth()] +' ' + msgdate.getDate() + ' ' + (msgdate.getYear() + 1900) + ' ' + msgHours + ':'+ msgMins +'; ';
+      msg = formattedDate + socket.thisUser + ": " + msg;
+      var colorFormat = '<span style=color:rgb(' + socket.color[0] + ',' + socket.color[1] + ',' + socket.color[2] + ')>';
+      console.log('message: ' + msg); //Could save the message with the formatting if needed.
+      chatHistory.push(msg);
+      socket.broadcast.emit('chat message', colorFormat + msg + '</span>');
+      socket.emit('chat message', '<b>' +colorFormat + msg +'</span></b>' );
+    }
+
+    //Final Stuff
     console.log('Chat History Length: ' + chatHistory.length);
     if (chatHistory.length > 200) {
       chatHistory.shift(); //Remove from front of array;
@@ -54,6 +118,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
+http.listen(port, function(){
   console.log('listening on *:3000');
 });
